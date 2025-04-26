b10e239136cc301e7da895ed2222f7c4b0cc9c49
Leonel97 <ronnyportillo59@gmail.com>
2025-04-26 17:36:35 -0600

    cargando componente principal

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const app = () =>{
+
+    return(
+        <div>
+            <h1>
+                Hola Mundo Desde React
+            </h1>
+            <p>
+                Creando mi primera aplicaicion desde react
+            </p>
+
+        </div>
+    );
+}
+
+export default app;
